f3ec309f83994b5c6398785db69a320294119db6
Blanc <leonardogabito00000@gmail.com>
2024-08-16 21:45:32 +0800

    Complete Rock Paper and Scissor game with AI

--- rockpaperscissor/script.js
+++ rockpaperscissor/script.js
@@ -1,20 +1,62 @@
 const humanBtn = document.querySelectorAll('.human .btn');
 
-humanBtn.forEach( function(btn) {
+let human;
+
+humanBtn.forEach( function(btn, index) {
     btn.addEventListener('click',function() {
         humanBtn.forEach( function(button){
             button.classList.remove('highlight');
         });
         btn.classList.add('highlight');
+        human = index;
     });
 });
-
-let rand = () => Math.floor(Math.random() * 4);
 
 const selected = document.getElementById('select');
 
 selected.addEventListener('click', function() {
-    console.log(rand());
+    let rand = Math.floor(Math.random() * 3);
+    highlightBtn(rand);
+    defineWinner(human, rand);
 });
 
+const robotBtn = document.querySelectorAll('.robot .btn');
 
+function highlightBtn(rand){
+    robotBtn.forEach( function(btn) {
+        let a = 0;
+        robotBtn.forEach( function(button){
+            if(a===rand){
+                button.classList.add('highlight');
+            }else{
+                button.classList.remove('highlight');
+            }
+            a++;
+        });
+        
+    });
+}
+
+const wOrl = document.getElementById('winORlose');
+
+function defineWinner(humanNum, robotNum){
+    if(humanNum === 0 && robotNum === 0){
+        wOrl.textContent = "DRAW";
+    }else if(humanNum === 0 && robotNum === 1){
+        wOrl.textContent = "ROBOT WINS";
+    }else if(humanNum === 0 && robotNum === 2){
+        wOrl.textContent = "HUMAN WINS";
+    }else if(humanNum === 1 && robotNum === 0){
+        wOrl.textContent = "HUMAN WINS";
+    }else if(humanNum === 1 && robotNum === 1){
+        wOrl.textContent = "DRAW";
+    }else if(humanNum === 1 && robotNum === 2){
+        wOrl.textContent = "ROBOT WINS";
+    }else if(humanNum === 2 && robotNum === 0){
+        wOrl.textContent = "ROBOT WINS";
+    }else if(humanNum === 2 && robotNum === 1){
+        wOrl.textContent = "HUMAN WINS";
+    }else if(humanNum === 2 && robotNum === 2){
+        wOrl.textContent = "DRAW";
+    }
+}
